e14531ba8cfd3055171affab8b5a92d5389bf716
John Apostol <notimetobeidle@gmail.com>
2016-12-16 17:08:19 -0600

    Create some containers

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -6,19 +6,19 @@ import type { LinkType } from '../types';
 const Link = ({ active, children, onClick }: LinkType) => {
   if (active) {
     return <span>{children}</span>;
-  } else {
-    return (
-      <a
-        onClick={(e) => {
-          e.preventDefault();
-          onClick();
-        }}
-        tabIndex="0"
-      >
-        {children}
-      </a>
-    );
   }
+
+  return (
+    <a
+      onClick={(e) => {
+        e.preventDefault();
+        onClick();
+      }}
+      tabIndex="0"
+    >
+      {children}
+    </a>
+  );
 };
 
 export default Link;
